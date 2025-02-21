6b06a9b74e183058a775f40eddc9c9b4c7a73085
Ari <24855858+AriFarj@users.noreply.github.com>
2025-02-21 13:23:45 +0200

    base menu done

--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,21 @@
+import image1 from "./img/food1.jpg"
+
 export function loadMenu() {
 
     let menuItems = [
         {
             name: "Fancy Meal 1",
-            image: null,
+            image: image1,
             price: "59.99"
         },
         {
             name: "Fancy Meal 2",
-            image: null,
+            image: image1,
             price: "69.99"
         },
         {
             name: "Fancy Meal 3",
-            image: null,
+            image: image1,
             price: "79.99"
         },
     ]
@@ -24,4 +26,30 @@ export function loadMenu() {
 
     const menuSection = document.createElement('div');
     content.appendChild(menuSection)
+
+    const menuHeader = document.createElement('h1');
+    menuHeader.innerText = "Check out our menu!";
+    menuSection.appendChild(menuHeader);
+
+    menuItems.forEach( (item) => {
+        let itemContainer = document.createElement('div');
+        itemContainer.classList.add('menu-item')
+
+        let name = document.createElement("h2");
+        name.innerText = item.name;
+
+        let price = document.createElement('p');
+        price.innerText = `$ ${item.price}`
+
+        let image = document.createElement('img');
+        image.src = `${item.image}`;
+        image.width = '300';
+        image.height= '300'
+
+        itemContainer.appendChild(image);
+        itemContainer.appendChild(name);
+        itemContainer.appendChild(price);
+        
+        menuSection.appendChild(itemContainer);
+    })
 }
