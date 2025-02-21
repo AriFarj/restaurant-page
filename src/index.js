53f2e12a74dba13f8ba4823c7a4155902b1358b0
Ari <24855858+AriFarj@users.noreply.github.com>
2025-02-21 10:24:51 +0200

    event listeners f or buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./style.css";
 import { loadHome } from "./home";
+
+btnEventListeners();
 loadHome();
+
+function btnEventListeners() {
+    const homePage = document.querySelector(".home")
+    const menuPage = document.querySelector(".menu")
+    const aboutPage = document.querySelector(".about")
+
+    homePage.addEventListener("click", loadHome);
+}
